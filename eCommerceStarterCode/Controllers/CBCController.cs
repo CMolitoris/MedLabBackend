529f80655f9db130057838b6cc6a0ad947c8c6f5
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-11-03 17:39:03 -0400

    CBC controller created and Condition controller updated

---
+++ eCommerceStarterCode/Controllers/CBCController.cs
@@ -0,0 +1,96 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/CBC/")]
+    [ApiController]
+    public class CBCController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CBCController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //-- GET api/CBC/ --//
+        [HttpGet]
+        public IActionResult GetAllTests()
+        {
+            var tests = _context.CBCs;
+            return Ok(tests);
+        }
+
+        //-- POST api/CBC/{userId} --//
+        [HttpPost("{userId}")]
+        public IActionResult PostTest(string userId, [FromBody] CBC newCBC)
+        {
+            CBC cbcToAdd = newCBC;
+            _context.CBCs.Add(cbcToAdd);
+            _context.SaveChanges();
+
+            _context.CBCLists.Add(new CBCList { UserId = userId, CBCId = cbcToAdd.Id });
+            _context.SaveChanges();
+
+            return StatusCode(201, cbcToAdd);
+        }
+
+        //-- GET api/CBC/{testId} --//
+        [HttpGet("{testId}/")]
+        public IActionResult GetIdOrders(int testId)
+        {
+            var selectedTest = _context.CBCs.Where(c => c.Id == testId).SingleOrDefault();
+            return Ok(selectedTest);
+        }
+
+        //-- PUT api/CBC/edit/{testId} --//
+        [HttpPut("edit/{testId}/")]
+        public IActionResult UpdateCBC(int testId, [FromBody] CBC value)
+        {
+            var test = _context.CBCs.Where(o => o.Id == testId).SingleOrDefault();
+            if (test == null)
+            {
+                return NotFound("Requested order not found");
+            }
+
+            test.Rbc = value.Rbc;
+            test.Hb = value.Hb;
+            test.Hct = value.Hct;
+            test.MCV = value.MCV;
+            test.MCH = value.MCH;
+            test.MCHC = value.MCHC;
+            test.RDW = value.RDW;
+            test.WBC = value.WBC;
+            test.Neu = value.Neu;
+            test.Lym = value.Lym;
+            test.Mon = value.Mon;
+            test.Eos = value.Eos;
+            test.Bas = value.Bas;
+            test.Plt = value.Plt;
+
+
+            _context.CBCs.Update(test);
+            _context.SaveChanges();
+            return StatusCode(201, test);
+        }
+
+        //-- DELETE api/CBC/delete/{testId}/ --//
+        [HttpDelete("delete/{testId}/")]
+        public IActionResult DeleteCar(int testId)
+        {
+            CBC test = _context.CBCs.Where(c => c.Id == testId).SingleOrDefault();
+            _context.CBCs.Remove(test);
+            _context.SaveChanges();
+            return Ok(test);
+        }
+
+    }
+}
---
+++ eCommerceStarterCode/Controllers/Condition.cs
@@ -0,0 +1,68 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/Condition/")]
+    [ApiController]
+    public class ConditionController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ConditionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //-- GET api/Condition/ --//
+        [HttpGet]
+        public IActionResult GetAllConditions()
+        {
+            var conditions = _context.Conditions;
+            return Ok(conditions);
+        }
+
+        //-- POST api/Condition/ --//
+        [HttpPost]
+        public IActionResult PostCondition([FromBody] Condition newCondition)
+        {
+            Condition conditionToAdd = newCondition;
+            _context.Conditions.Add(conditionToAdd);
+            _context.SaveChanges();
+
+            return StatusCode(201, conditionToAdd);
+        }
+
+
+        //-- PUT api/Condition/edit/{conditionId} --//
+        [HttpPut("edit/{conditionId}/")]
+        public IActionResult UpdateCondition(int conditionId, [FromBody] Condition value)
+        {
+            var condition = _context.Conditions.Where(o => o.Id == conditionId).SingleOrDefault();
+            if (condition == null)
+            {
+                return NotFound("Requested order not found");
+            }
+
+            return StatusCode(201, condition);
+        }
+
+        //-- DELETE api/Condition/delete/{conditionId}/ --//
+        [HttpDelete("delete/{conditionId}/")]
+        public IActionResult DeleteCondition(int conditionId)
+        {
+            Condition condition = _context.Conditions.Where(c => c.Id == conditionId).SingleOrDefault();
+            _context.Conditions.Remove(condition);
+            _context.SaveChanges();
+            return Ok(condition);
+        }
+
+    }
+}
